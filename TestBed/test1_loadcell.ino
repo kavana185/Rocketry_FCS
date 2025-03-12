4c1f73c080b8228fbf6e00b9e7ba12412389ea7c
Kavana M A <153212099+kavana185@users.noreply.github.com>
2025-03-12 17:40:34 +0530

    Update test1_loadcell.ino

--- TestBed/test1_loadcell.ino
+++ TestBed/test1_loadcell.ino
@@ -1,1 +1,31 @@
+#include "HX711.h"
 
+// HX711 circuit wiring
+#define LOADCELL_DOUT_PIN1  3
+#define LOADCELL_SCK_PIN    13
+// #define LOADCELL_DOUT_PIN2  5
+
+HX711 scale1;
+// HX711 scale2;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Initialize the first HX711 module
+  scale1.begin(LOADCELL_DOUT_PIN1, LOADCELL_SCK_PIN);
+  // Initialize the second HX711 module
+  // scale2.begin(LOADCELL_DOUT_PIN2, LOADCELL_SCK_PIN);
+}
+
+void loop() {
+  if (scale1.is_ready()) {
+    long reading1 = scale1.read();
+    Serial.print("Reading 1: ");
+    //Gives readings in newtons
+    Serial.println(((reading1+11830.91432490605)/-10.38429025)/1000-0.15);
+    //Serial.println(reading1);
+  } else {
+    Serial.println("HX711 not found (scale 1).");
+  }
+  delay(1000); // Wait for a second
+}
